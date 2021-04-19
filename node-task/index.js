25d69ae0eabf51eb0ca485a6524c18033e0cf1e5
harleyquinn05 <81037032+harleyquinn05@users.noreply.github.com>
2021-04-19 10:07:54 -0700

    Zuri Node Js Task

---
+++ node-task/index.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const fetchData = async () => {
+    try {
+        const url = "http://jsonplaceholder.typicode.com/posts"
+        const res = await axios.get(url)
+        const result = JSON.stringify(res.data, null, 2);
+        fs.writeFile("./result/posts.json", result, (err) => {
+            if (err) throw err;
+            console.log("write data to file complete");
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+fetchData();
